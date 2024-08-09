0e804520f0b495edd92545f715568c827ee59c48
Gustavo Henrique <gustavoh.santos735@gmail.com>
2024-08-08 22:52:12 -0300

    chore: add global styles

--- src/views/layouts/default.php
+++ src/views/layouts/default.php
@@ -1,14 +1,25 @@
 <!DOCTYPE html>
-<html lang="pt-BR">
+<html lang="pt-br">
 
 <head>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href=<?= url("src/views/assets/styles/global.css") ?>>
+    <link rel="stylesheet" href="../assets/styles/header.css">
     <title><?= $this->e($title) ?></title>
 </head>
 
 <body>
+    <header class="header"></header>
     <?= $this->section('content') ?>
+
+    <script src="https://unpkg.com/lucide@latest"></script>
+    <script>
+        lucide.createIcons();
+    </script>
 </body>
 
 </html>
